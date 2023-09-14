d86d64d5bd45b9931d500b3eaf768817c4ec25a4
Garrett <lamgarr@outlook.com>
2023-09-13 19:51:27 -0700

    Add user model

---
+++ demomysql/src/main/java/com/example/lamgarr/demomysql/models/User.java
@@ -0,0 +1,13 @@
+package com.example.lamgarr.demomysql.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private int id;
+    private String name;
+}
