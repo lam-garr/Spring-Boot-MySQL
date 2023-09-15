912f3f2beba211fc66cc8a348cf37e52f1888bc5
Garrett <lamgarr@outlook.com>
2023-09-14 21:44:54 -0700

    Add UserRepository interface

---
+++ demomysql/src/main/java/com/example/lamgarr/demomysql/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.lamgarr.demomysql.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.lamgarr.demomysql.models.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+    
+}
