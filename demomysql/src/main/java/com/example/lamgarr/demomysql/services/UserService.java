9411ceb7c1bbd47987097802e1abeda1e9fa9463
Garrett <lamgarr@outlook.com>
2023-09-20 21:43:27 -0700

    Add service for saving a user

--- demomysql/src/main/java/com/example/lamgarr/demomysql/services/UserService.java
+++ demomysql/src/main/java/com/example/lamgarr/demomysql/services/UserService.java
@@ -2,9 +2,14 @@ package com.example.lamgarr.demomysql.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.example.lamgarr.demomysql.models.User;
 import com.example.lamgarr.demomysql.repository.UserRepository;
 
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
 }
