ee00f3b6a7c4971e01c7cc859ddc3982fd38f273
Garrett <lamgarr@outlook.com>
2023-09-22 18:45:48 -0700

    Add controller for saving a user

--- demomysql/src/main/java/com/example/lamgarr/demomysql/controllers/UserController.java
+++ demomysql/src/main/java/com/example/lamgarr/demomysql/controllers/UserController.java
@@ -1,10 +1,18 @@
 package com.example.lamgarr.demomysql.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.lamgarr.demomysql.models.User;
 import com.example.lamgarr.demomysql.services.UserService;
 
+@RestController
 public class UserController {
     @Autowired
     private UserService userService;
+
+    public User saveUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
 }
