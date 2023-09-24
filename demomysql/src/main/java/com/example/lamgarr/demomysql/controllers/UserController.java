c171bb226b6484e2bcb9ca672b2c18c4a88b9df8
Garrett <lamgarr@outlook.com>
2023-09-23 18:49:39 -0700

    Add controller for saving multiple users

--- demomysql/src/main/java/com/example/lamgarr/demomysql/controllers/UserController.java
+++ demomysql/src/main/java/com/example/lamgarr/demomysql/controllers/UserController.java
@@ -1,6 +1,9 @@
 package com.example.lamgarr.demomysql.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +17,10 @@ public class UserController {
 
     public User saveUser(@RequestBody User user) {
         return userService.saveUser(user);
+    }
+
+    @PostMapping("/save-users")
+    public List<User> saveUsers(@RequestBody List<User> users) {
+        return userService.saveUsers(users);
     }
 }
