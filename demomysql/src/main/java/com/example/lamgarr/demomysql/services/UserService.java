7f3ca6fdc493e9f610a92aa22cf13709f67b0965
Garrett <lamgarr@outlook.com>
2023-10-13 21:10:19 -0700

    Add service for deleting user by email

--- demomysql/src/main/java/com/example/lamgarr/demomysql/services/UserService.java
+++ demomysql/src/main/java/com/example/lamgarr/demomysql/services/UserService.java
@@ -35,4 +35,9 @@ public class UserService {
         userRepository.deleteById(id);
         return;
     }
+
+    public void deleteUserByEmail(String email) {
+        //
+        return;
+    }
 }
